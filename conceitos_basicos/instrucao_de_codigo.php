508bdb8e6ea1e1db5c88db3e9181d3ff229f6f6c
IanPC <iancaminhas25@hotmail.com>
2024-01-08 19:59:03 -0300

    Palavras reservadas

--- conceitos_basicos/instrucao_de_codigo.php
+++ conceitos_basicos/instrucao_de_codigo.php
@@ -3,7 +3,6 @@
     echo "Uma linha <br>";
     echo "Outra de uma linha <br>";
 
-    
     if(5>2) {
         echo "Dentro do if <br>";
     }
---
+++ conceitos_basicos/palavras_reservadas.php
@@ -0,0 +1,15 @@
+<?php 
+    /*
+        Todas as funções abaixo irão gerar
+        um erro no navegador, pois os nomes dessas funções 
+        coincidem com as palavras reservadas do próprio PHP.
+    */
+    function echo() {
+        return true;
+    }
+
+    function while() {
+        return true;
+    }
+
+?>
