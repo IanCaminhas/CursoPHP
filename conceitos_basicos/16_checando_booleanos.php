47f94967df4042a949bbf95c9cbed77a28b0473a
IanPC <iancaminhas25@hotmail.com>
2024-01-09 18:21:13 -0300

    checando booleanos

---
+++ conceitos_basicos/16_checando_booleanos.php
@@ -0,0 +1,24 @@
+<?php 
+    $a = true;
+    
+    if(is_bool($a)) { //true
+        echo "É um booleano 1<br>";
+    }
+
+    if(is_bool(0)) { //false, pois a função considera 0 como um número.
+        echo "É um booleano 2<br>";
+    }
+
+    if(is_bool(false)) { //false
+        echo "É um booleano 3<br>";
+    }
+
+    if(0 == false) { //true
+        echo "0 é considerado falso! <br>";
+    }
+
+    
+
+
+
+?>
