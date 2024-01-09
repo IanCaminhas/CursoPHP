76df4eb6f66130460ffcf51f554d965863491bdc
IanPC <iancaminhas25@hotmail.com>
2024-01-09 17:18:06 -0300

    strings

---
+++ conceitos_basicos/13_strings.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Testando texto de aspas duplas <br>";
+
+    echo 'Testando texto de aspas simples <br>';
+
+    //Usando as aspas simples e duplas
+    echo "Ele disse: 'Olá!' <br>";
+    echo 'Ele disse: "Olá!" <br>';
+
+    $idade = 15;
+    echo "Ele tem $idade anos <br>";
+
+    //Aspas simples não leem variáveis
+    echo 'Ele tem $idade anos';
+
+
+?>
