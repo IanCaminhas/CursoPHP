05e4c1644483e1cf9fcadc448cb9e1acf7408673
IanPC <iancaminhas25@hotmail.com>
2024-01-09 18:06:27 -0300

    booleanos

---
+++ conceitos_basicos/15_booleanos.php
@@ -0,0 +1,22 @@
+<?php
+    //O navegador retorna 1
+    echo true;
+    echo "<br>";
+
+    //O navegador não retorna nada 
+    echo false;
+
+    if(true) { //true
+        echo "É verdadeiro 1<br>";
+    }
+
+    if(5>2){ //true
+        echo "É verdadeiro 2<br>";
+    }
+
+    $podeEntrar = true;
+
+    if($podeEntrar) {
+        echo "O usuario pode entrar";
+    }
+?>
