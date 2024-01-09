c7130a166650747001990513b4ae4e791ab567f0
IanPC <iancaminhas25@hotmail.com>
2024-01-09 16:59:34 -0300

    float

---
+++ conceitos_basicos/11_float.php
@@ -0,0 +1,19 @@
+<?php
+    $a = 1.12;
+    
+    echo $a;
+    
+    echo "<br>";
+    
+    echo 1.123;
+    
+    echo "<br>";
+    
+    echo 12.5 + 1.3278;
+    
+    echo "<br>";
+    
+    //Operações com tipos distintos
+    echo 12 + 1.3278;
+
+?>
