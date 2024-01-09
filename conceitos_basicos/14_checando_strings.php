764ba7dbe6f25d78b0938648da375ffe9905fe4c
IanPC <iancaminhas25@hotmail.com>
2024-01-09 17:24:45 -0300

    checando strings

---
+++ conceitos_basicos/14_checando_strings.php
@@ -0,0 +1,18 @@
+<?php
+
+    $str = "Ian";
+    $num = 12;
+
+    if(is_string($str)) {
+        echo "$str é uma string 1<br>";
+    }
+
+    if(is_string($num)) {
+        echo "$str é uma string 2<br>";
+    }
+
+    if(is_string("asd")) {
+        echo "É uma string 3<br>";
+    }
+
+?>
