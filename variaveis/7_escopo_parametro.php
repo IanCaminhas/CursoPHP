b0f779735ad45044ec7016efa4cd2c5a41fcb985
IanPC <iancaminhas25@hotmail.com>
2024-01-10 14:25:51 -0300

    Escopo parametro

---
+++ variaveis/7_escopo_parametro.php
@@ -0,0 +1,14 @@
+<?php
+
+    function soma($a,$b) {
+        echo $a + $b;
+        echo "<br>";
+    }
+
+    //Saída: 6
+    soma(2,4);
+    //Saída: 14
+    soma(6,8);
+    //Saída: 20
+    soma(10,10);
+?>
