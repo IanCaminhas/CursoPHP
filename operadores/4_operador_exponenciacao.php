5480b3b06678e7d14ff63f572c6d46065b26ea3a
IanPC <iancaminhas25@hotmail.com>
2024-01-10 16:40:39 -0300

    Operador exponenciacao

---
+++ operadores/4_operador_exponenciacao.php
@@ -0,0 +1,22 @@
+<?php
+    //Saída: 25
+    echo 5 ** 2;
+    echo "<br>";
+    //Saída: 1024
+    echo 2 ** 10;
+    echo "<br>";
+
+    $a = 7;
+    $b = 4;
+
+    //Saída: 2401
+    echo $a ** $b;
+    echo "<br>";
+
+    $c = 12;
+    //Saída: 35831808
+    $d = $c ** $a;
+
+    echo $d;
+
+?>
