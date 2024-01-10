e6d4b389f4c83e96b83edd42ab995959966d77a5
IanPC <iancaminhas25@hotmail.com>
2024-01-10 16:15:21 -0300

    Operadores aritmeticos

---
+++ operadores/2_operadores_aritmeticos.php
@@ -0,0 +1,18 @@
+<?php 
+    $soma = 5 + 5;
+    //Saída: 10
+    echo $soma . "<br>";
+    $subtracao = 10 - 2;
+    //Saída: 8
+    echo $subtracao . "<br>";
+    $multi = 2 * 199;
+    //Saída: 398
+    echo $multi ."<br>";
+    $divisao = 123 / 48;
+    //Saída: 2,5625
+    echo $divisao . "<br>";
+
+    $op = 5 + (2 / 7) * 8;
+    //Saída: 7.2857142857143
+    echo $op ."<br>";
+?>
