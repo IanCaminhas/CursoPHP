f27d8395cb7dba8944ac3812e324b71b445ce28b
IanPC <iancaminhas25@hotmail.com>
2024-01-10 12:40:45 -0300

    Variavel de variavel

---
+++ variaveis/2_var_de_var.php
@@ -0,0 +1,11 @@
+<?php
+
+    $x = "nome"; //valor de nome
+    //Saída: nome
+    echo "$x <br>";
+
+    $$x= "Ian"; //var com o nome de x(nome), com valor de Ian. $$x agora é $nome
+    //Saída: Ian
+    echo "$nome <br>";
+
+?>
