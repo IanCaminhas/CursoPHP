bb0e008a8129f4ad71793f8176d87ae5c3c9d7f6
IanPC <iancaminhas25@hotmail.com>
2024-01-10 16:26:24 -0300

    Operador modulo

---
+++ operadores/3_operador_modulo.php
@@ -0,0 +1,15 @@
+<?php 
+    echo 12 % 2;
+    //Saída: 0 Módulo
+    echo " Módulo <br>";
+    echo 12/2;
+    //Saída: 6 Divisão
+    echo " Divisão <br>";
+
+    //Saída: 1
+    echo 13 % 2;
+    echo "<br>";
+    //Saída: 3
+    echo 13 % 5;
+    echo "<br>";
+?>
