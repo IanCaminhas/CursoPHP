67e7f60befecaa35798ecce3fc7e0d66bc3ee940
IanPC <iancaminhas25@hotmail.com>
2024-01-10 12:31:35 -0300

    Introducao a variaveis

---
+++ variaveis/1_introducao.php
@@ -0,0 +1,30 @@
+<?php
+
+    $teste = "Algum  texto";
+    //Saída: Algum texto
+    echo $teste;
+
+    $num = 10;
+
+    echo "<br>";
+    //Saída: 10
+    echo $num;
+
+    $_nome = "Ian";
+
+    echo "<br>";
+    //Saída: Ian
+    echo $_nome;
+
+    echo "<br>";
+
+    $Nome = "Cristiano Ronaldo";
+    //Saída: Cristiano Ronaldo
+    echo $Nome;
+
+    //Estilos para variáveis que possuem mais de uma palavra. CamelCase ou separado por _
+    $velocidadeMaxima = 100;
+    $velocidade_minima = 100;
+
+
+?>
