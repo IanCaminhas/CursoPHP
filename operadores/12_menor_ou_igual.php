697f04430cddcf0c8a9e0e1d5c10fd95a01c4612
IanPC <iancaminhas25@hotmail.com>
2024-01-11 08:34:49 -0300

    Operador menor ou igual e menor

---
+++ operadores/12_menor_ou_igual.php
@@ -0,0 +1,34 @@
+<?php
+    //4 é menor que 12
+    if(4 < 12) {
+        echo "4 é menor que 12<br>";
+    }
+
+    //Saída: sem saída no navegador.
+    if(24 < 12) {
+        echo "24 é menor que 12<br>";
+    }
+
+    //Saída: 12 é menor ou igual 12
+    if(12 <= 12) {
+        echo "12 é menor ou igual 12<br>";
+    }
+
+    $a = 5;
+    $b = 18;
+
+    //Saída: A é menor ou igual a B
+    if($a <= $b) {
+        echo "A é menor ou igual a B<br>";
+    }
+
+    //Saída: sem saída no navegador.
+    if($b <= $a) {
+        echo "B é menor ou igual a A<br>";
+    }
+
+     //B é menor ou igual a 42
+     if($b <= 42) {
+        echo "B é menor ou igual a 42<br>";
+    }
+?>
