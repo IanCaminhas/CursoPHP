018b66e3b0bc960214ff6939cc05b6a04bcb6cdc
IanPC <iancaminhas25@hotmail.com>
2024-01-12 14:32:18 -0300

    Utilizando o include

---
+++ inclusao_de_codigo/1_include/index.php
@@ -0,0 +1,9 @@
+<?php
+    //Opbjeto é usar os recursos que estão no arquivo teste.php
+    //Consegui usar o valor da variável $c que está em teste.php
+    include "teste.php";
+
+?>
+
+<p>Após o include</p>
+<p>Imprimindo c <?php echo $c; ?></p>
