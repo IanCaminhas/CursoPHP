fa7bb4a02fc12191f4b2440f92b36317db2dea66
IanPC <iancaminhas25@hotmail.com>
2024-01-13 16:56:16 -0300

    Criando e invocando funcoes

---
+++ funcoes/1_chamando_funcoes.php
@@ -0,0 +1,21 @@
+<?php
+
+    function soma(){
+
+        $a = 5;
+        $b = 4;
+
+        echo $a + $b . "<br>";
+    }
+
+    //Chamando / invocando funcao
+    //Saída: 9
+    soma();
+    //Saída: 9
+    soma();
+
+    //Funcao do PHP que deixam as letras maiúsculas de uma palavra
+    //Saída: TESTANDO FUNçãO
+    echo strtoupper("testando função");
+
+?>
