b4dd5a68aff75e9d631eaae6fa92f20dc87d1629
IanPC <iancaminhas25@hotmail.com>
2024-01-13 18:36:21 -0300

    Multiplos retornos

---
+++ funcoes/7_multiplos_retornos.php
@@ -0,0 +1,20 @@
+<?php
+
+    function alteraDados($nome, $idade) {
+
+        $nome = "Sr . $nome";
+        $idade = "$idade anos";
+
+        return [$nome, $idade];
+    }
+
+    $dados = alteraDados("Ian", 29);
+    //saída: Array ( [0] => Sr . ian [1] => 29 anos )
+    print_r($dados);
+    
+    echo "<br>";
+    //Olá Sr . Ian, você tem 29 anos
+    echo "Olá $dados[0], você tem $dados[1]"
+
+
+?>
