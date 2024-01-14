44fbed708f1bd16432b80477c7910bec59e664b4
IanPC <iancaminhas25@hotmail.com>
2024-01-14 07:07:09 -0300

    Interpolacao

---
+++ strings/1_interpolacao.php
@@ -0,0 +1,15 @@
+<?php
+
+    //O ideal é manter apenas com uma das sintaxes
+
+    $nome = "Ian";
+    $idade = 29;
+    $profissao = "Estudante";
+
+    //Saída: Eu sou o Ian, tenho 29 anos e atuo como Estudante
+    echo "Eu sou o $nome, tenho $idade anos e atuo como {$profissao} <br>";
+    /*Segunda forma de realizar a interpolação:
+    Saída: Eu sou o Ian, tenho 29 anos e atuo como Estudante */
+    echo "Eu sou o $nome, tenho $idade anos e atuo como {$profissao} <br>";
+
+?>
