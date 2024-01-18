6c8c6de466ee0ebca84d0aa99c380fdcd733a65f
IanPC <iancaminhas25@hotmail.com>
2024-01-18 17:55:31 -0300

    Traits

---
+++ orientacao_objetos/11_traits.php
@@ -0,0 +1,33 @@
+<?php
+
+    trait Objeto {
+
+        public $y = 10;
+        public function teste() {
+            echo "Testando trait de objeto <br>";
+        }
+    }
+
+    trait Testando {
+
+        public function traitTeste() {
+            echo "Este método é da trait Testando <br>";
+        }
+    }
+
+    class Central {
+        //Posso usar diversas traits
+        use Objeto;
+        use Testando;
+    }
+
+    $x = new central;
+    //Saída: Testando trait de objeto
+    $x->teste();
+    //Saída: Este método é da trait Testando
+    $x->traitTeste();
+    //Saída: 10
+    echo $x->y . "<br>";
+
+
+?>
